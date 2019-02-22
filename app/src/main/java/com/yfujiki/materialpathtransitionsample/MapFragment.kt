64276847437bb06df5c99e7d0679aed14b38b891
Yuichi Fujiki <yuichi@chaione.com>
2019-02-22 13:55:04 +0800

    Show better path.

--- app/src/main/java/com/yfujiki/materialpathtransitionsample/MapFragment.kt
+++ app/src/main/java/com/yfujiki/materialpathtransitionsample/MapFragment.kt
@@ -22,14 +22,17 @@ class MapFragment(val listener: MapFragmentListener): Fragment() {
         view.pathCanvas.background = drawable
 
         drawable.startAnimating(listOf(
-            NormalizedPoint(0.1f, 0.7f),
-            NormalizedPoint(0.15f, 0.4f),
-            NormalizedPoint(0.26f, 0.6f),
-            NormalizedPoint(0.27f, 0.5f),
+            NormalizedPoint(0.1f, 0.74f),
+            NormalizedPoint(0.15f, 0.68f),
+            NormalizedPoint(0.26f, 0.69f),
+            NormalizedPoint(0.27f, 0.72f),
             NormalizedPoint(0.29f, 0.7f),
             NormalizedPoint(0.35f, 0.72f),
-            NormalizedPoint(0.7f, 0.3f),
-            NormalizedPoint(0.8f, 0.1f)
+            NormalizedPoint(0.45f, 0.72f),
+            NormalizedPoint(0.5f, 0.66f),
+            NormalizedPoint(0.7f, 0.6f),
+            NormalizedPoint(0.72f, 0.5f),
+            NormalizedPoint(0.8f, 0.4f)
         ))
 
         view.button.setOnClickListener({
